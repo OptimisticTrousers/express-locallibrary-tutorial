80cbba584e83d3e70d1288402e3ea9034d794064
OptimisticTrousers <tony.isern@yahoo.com>
2022-10-07 15:30:40 -0400

    Running the 'compression' middleware

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var catalogRouter = require("./routes/catalog");
 //Import routes for "catalog" area of site
+const compression = require("compression");
 
 var app = express();
 const mongoose = require("mongoose");
@@ -21,6 +22,7 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+app.use(compression());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
